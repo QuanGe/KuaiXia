e8076eb9c4b844eec65c72a1bef533645a057f99
ShuailongWang <1250626642@qq.com>
2020-11-25 22:48:06 +0800

    none

---
+++ DownloadTool/DownloadTool/Module/Browser/View/DTBrowserTabView.h
@@ -0,0 +1,17 @@
+//
+//  DTBrowserTabView.h
+//  DownloadTool
+//
+//  Created by WSL on 2020/11/25.
+//  Copyright © 2020 DownloadTool. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DTBrowserTabView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DownloadTool/DownloadTool/Module/Browser/View/DTBrowserTabView.m
@@ -0,0 +1,32 @@
+//
+//  DTBrowserTabView.m
+//  DownloadTool
+//
+//  Created by WSL on 2020/11/25.
+//  Copyright © 2020 DownloadTool. All rights reserved.
+//
+
+#import "DTBrowserTabView.h"
+
+@interface DTBrowserTabView()
+
+@end
+
+@implementation DTBrowserTabView
+
+- (instancetype)initWithFrame:(CGRect)frame{
+    if (self = [super initWithFrame:frame]) {
+        [self setupTabViewUI];
+    }
+    return self;
+}
+
+- (void)setupTabViewUI{
+    
+}
+
+
+#pragma mark - Lazy
+
+
+@end
